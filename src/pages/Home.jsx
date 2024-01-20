4ed4d98b5f287ad534e7144373ede679e22ef6cc
OpiniIsaac <isaacopini8@gmail.com>
2024-01-20 12:15:45 +0300

    started on side bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from 'react'
+import SideBar from '../components/SideBar'
 
-const Home = () => {
-     return (
-          <div>
-               <h1>Helloe</h1>
-          </div>
-     );
+export default function Home() {
+  return (
+    <>
+    <SideBar/>
+    </>
+  )
 }
-
-export default Home;
